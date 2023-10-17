918dff683f721e95be2591e145240ebc972d1567
Gregory Rodriguez <glrd4726ml@gmail.com>
2023-10-17 06:42:03 -0300

    cambios2

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
-const servidor = require("./Server");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const rutas_productos = require('./routes/productos')
+const rutas_usuarios = require('./routes/usuario')
+require("dotenv").config();
 const database = require("./conec");
+
+
+app.use(cors());
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(express.static("storage"))
+
+const puerto = process.env.PORT || 6000;
+
+const servidor = () =>{
+    console.log();
+    console.log("Conectando Servidor....")
+    console.log();
+    app.listen(puerto, ()=>{
+    console.log("El servidor esta conectado por el puerto: "+puerto)
+})}
+
+app.use("/api", rutas_productos)
+app.use("/api", rutas_usuarios);
 
 servidor();
 database();
+
